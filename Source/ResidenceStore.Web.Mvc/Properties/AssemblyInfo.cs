2577b632cf3a8f47501fbfd809c5b4e9be4c9d54
Tom Adriaenssen <tom@interfaceimplementation.be>
2013-04-04 18:54:12 +0200

    Fixed assemby infos for Web and Web.Mvc projects.

--- Source/ResidenceStore.Web.Mvc/Properties/AssemblyInfo.cs
+++ Source/ResidenceStore.Web.Mvc/Properties/AssemblyInfo.cs
@@ -6,31 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ResidenceStore.Web.Mvc")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ResidenceStore.Web.Mvc")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1adde538-cfe1-4da4-9425-b60e7bd97ffd")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
