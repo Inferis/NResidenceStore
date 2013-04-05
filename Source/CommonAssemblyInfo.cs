801ef58e677597e04ddde11cf07076273cc53544
Tom Adriaenssen <tom@interfaceimplementation.be>
2013-04-05 15:16:12 +0200

    Working on automatic versioning.

--- Source/CommonAssemblyInfo.cs
+++ Source/CommonAssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyDescription("")]
@@ -11,5 +10,3 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.2")]
