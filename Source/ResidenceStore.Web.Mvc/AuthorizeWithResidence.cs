8f3eee27725fd2ed3cf7ab8b775ecf9730fca7c7
Tom Adriaenssen <tom@interfaceimplementation.be>
2013-04-09 01:01:14 +0200

    Added AuthorizeWithResidence attribute for MVC

---
+++ Source/ResidenceStore.Web.Mvc/AuthorizeWithResidence.cs
@@ -0,0 +1,13 @@
+﻿namespace ResidenceStore.Web.Mvc
+{
+    public static class AuthorizeWithResidence
+    {
+        public static IResidenceStore Store { get; private set; }
+
+        public static void UseStore(IResidenceStore store)
+        {
+            Store = store;
+        }
+
+    }
+}
---
+++ Source/ResidenceStore.Web.Mvc/AuthorizeWithResidenceAttribute.cs
@@ -0,0 +1,40 @@
+﻿namespace ResidenceStore.Web.Mvc
+{
+    using System.Text.RegularExpressions;
+    using System.Web.Mvc;
+    using ResidenceStore;
+
+    public class AuthorizeWithResidenceAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            var header = filterContext.HttpContext.Request.Headers["Authorization"];
+
+            if (!string.IsNullOrEmpty(header) && header.StartsWith("Token ")) {
+                var match = Regex.Match(header, @"token=(['""])(.*?)\1");
+                string token = null;
+                if (match.Success)
+                    token = match.Groups[2].Value;
+
+                if (!string.IsNullOrEmpty(token)) {
+                    var provider = filterContext.Controller as IResidenceStoreProvider;
+                    IResidenceStore store = null;
+                    if (provider != null) store = provider.ResidenceStore;
+                    if (store == null)
+                        store = AuthorizeWithResidence.Store;
+
+                    if (store != null) {
+                        var residence = store.ResidenceWithAuthorizationToken(token);
+                        if (residence != null) {
+                            return; // OK
+                        }
+                    }
+                }
+            }
+
+            base.OnAuthorization(filterContext);
+        }
+
+    }
+
+}
